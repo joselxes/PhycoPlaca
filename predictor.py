cb7fc3eb42666467b17d62d95d9fd86c8a9b8ac8
joselxes <fran3te@hotmail.com>
2022-06-07 22:48:31 -0500

    solution implementation

--- predictor.py
+++ predictor.py
@@ -1,30 +1,58 @@
 import datetime
-
+import sys
 plateToDay={ "1":"lunes",
         "2":"lunes",
         "3":"martes",
         "4":"martes",
-        "5":"miércoles",
-        "6":"miércoles",
+        "5":"miercoles",
+        "6":"miercoles",
         "7":"jueves",
         "8":"jueves",
         "9":"viernes",
         "0":"viernes",
 }
+starTime1=datetime.time(7,0)
+endTime1=datetime.time(9,30)
+starTime2=datetime.time(16,0)
+endTime2=datetime.time(19,30)
+
+plateList=["lunes","martes","miercoles","jueves","viernes","sabado","domingo",]
 
 class autoQuery:
-    def __init__(self,plateNumber,plateDate,plateTime):
-        self.number=plateNumber
-        self.date=plateDate
-        self.time=plateTime
+    def __init__(self,queryData):
+        self.number=int(queryData[0])
+
+
+        year,month,day=map(int,queryData[1].split("-"))
+        self.currentDate=datetime.date(year,month,day)
+
+
+        hour,minutes=map(int,queryData[2].split(":"))
+        self.hour=datetime.time(hour,minutes)
+
+
     def __str__(self):
-        # format.{the car {self.number}, inthe Day {self.date}, at the time {self.time}  }
-        # 'The car with the plate {number}, inthe Day {date}, at the time {time}'.format(number=self.number, date=self.date, time=self.time)
-        return f'\tThe car with the plate {self.number}, in the Day {self.date}, at the time {self.time}'
+
+        return True #f'\tPlate {self.number}, in the Day {self.currentDate}, at the time {self.time}'
+
+
     def drivingTime(self):
-        print("\t",self.number%10)    
-        
-        # __ini__():
-# python3 
-#
+
+        dontDrive=plateToDay[str(self.number%10)]
+        todayIs=plateList[self.currentDate.weekday()]
+        sameDay=dontDrive==todayIs
+        inTime=(starTime1<= self.hour <= endTime1) or (starTime2<= self.hour <= endTime2)
+
+        return   sameDay and inTime
+
+
+
+def main():
+    data=sys.stdin.readline()
+    # data=sys.stdin.read() no
+    # data=input().split()
+    # data[0]=int(data[0])
+    print("999",data, type(data))
+    # a=autoQuery(data)
+main()
 
